2dae9a0429c0b8762335504ee6c1bb6d3d1cb7a9
WeedForJustice <chengshuan@pm.me>
2020-06-28 13:18:00 +0800

    Add uVision 5 projects

---
+++ src/sp.c
@@ -0,0 +1,5 @@
+#include <AT89X51.H>
+#include "../inc/common.h"
+#include "../inc/delay.h"
+
+
---
+++ tests/sp/main.c
@@ -0,0 +1,9 @@
+#include <AT89X52.H>
+
+typedef unsigned char u8;
+typedef char i8;
+
+void main()
+{
+
+}
