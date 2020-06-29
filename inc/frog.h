457c78c39fdf2d4ce98079ca76264eb839e1051a
WeedForJustice <chengshuan@pm.me>
2020-06-30 00:00:27 +0800

    Fix compatibility

--- inc/frog.h
+++ inc/frog.h
@@ -1,13 +1,20 @@
-#include <at89x52.h>
+#include <AT89X52.H>
 
 #ifndef FROG_H
 #define FROG_H
 
 #define SP P3_4
+#define DOTM_R P2
+#define DOTM_C P0
+
 typedef unsigned char u8;
 typedef unsigned short u16;
 typedef char i8;
 typedef short i16;
+
+void sp_init();
+
+void dotm_put(u8 buf[8]);
 
 #endif // FROG_H
 
