55b886c68d81bd9e91659e1af8b27309f2a88e26
WeedForJustice <chengshuan@pm.me>
2020-06-29 23:59:23 +0800

    Add dotm.c and sp.c

---
+++ src/dotm.c
@@ -0,0 +1,14 @@
+#include <AT89X52.H>
+#include "../inc/frog.h"
+
+void dotm_put(u8 buf[8])
+{
+    u8 i, j;
+
+    for (i = 0; i < 8; i++)
+    {
+        for (j = 0; j < 8; j++)
+        {
+        }
+    }
+}
---
+++ src/sp.c
@@ -0,0 +1,11 @@
+#include <AT89X52.H>
+#include "../inc/frog.h"
+
+void sp_init()
+{
+    TMOD = 0x11; // Timer 0, 1 are 16 bit mode
+    ET0 = ET1 = 1; // Enable timers' interrupt
+    EX0 = 1; // Enable external interrupt
+    IT0 = 1; // Set interrupt mode to falling edge
+    EA = 1; // Enable main interrupt flag
+}
