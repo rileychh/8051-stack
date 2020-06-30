eb075844ab2d72765b6f78fb9f88b0e111ff6d98
lrnbrad <lrnbrad@gmail.com>
2020-06-30 21:08:41 +0800

    fix something but still have some futal bug

--- src/sp.c
+++ src/sp.c
@@ -3,14 +3,26 @@
 
 void sp_init()
 {
-    TMOD = 0x11; // Timer 0, 1 are 16 bit mode
-    ET0 = ET1 = 1; // Enable timers' interrupt
-    EX0 = 1; // Enable external interrupt
-    IT0 = 1; // Set interrupt mode to falling edge
-    EA = 1; // Enable main interrupt flag
+  TMOD = 0x11;   // Timer 0, 1 are 16 bit mode
+  ET0 = ET1 = 1; // Enable timers' interrupt
+  EX0 = 1;       // Enable external interrupt
+  IT0 = 1;       // Set interrupt mode to falling edge
+  EA = 1;        // Enable main interrupt flag
 }
 
-void sp_play(u8 sound)
+unsigned char sp_play(u8 sound) //0 means you loss one but didn't lose, 1 means you get the point perfectly, 2 means you lose
 {
-
+  if (sound == 0)
+  {
+    pitch_pos = 24;
+  }
+  else if (sound == 1)
+  {
+    pitch_pos++;
+  }
+  else
+  {
+    pitch_pos = 25;
+  }
+  return pitch_pos;
 }
