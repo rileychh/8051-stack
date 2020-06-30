b2b5f9faee1107925563019bdacf4c1c58dd26d4
WeedForJustice <chengshuan@pm.me>
2020-06-30 12:29:37 +0800

    Fix Windows format

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #include <AT89X52.H>
-#include "../inc/frog.h" 
+#include "../inc/frog.h"
 
 u8 delaycount = 0;
 u8 pitch_pos = 0;
@@ -30,11 +30,10 @@ code u8 pitch_TL[] = {
     0x88, 0xF4, 0x59, 0xB8, 0x13, 0x68, 0xB8, 0x04, 0x4C, 0x8F, 0xCF, 0x0B,
     0x44, 0x7A, 0xAC, 0xDC, 0x09, 0x34, 0x5C, 0x82, 0xA6, 0xC7, 0xE7, 0x05};
 
-void delay ()
+void delay()
 {
-
 }
-		
+
 void onBtnPress() interrupt 0
 {
     EX0 = 0;
@@ -46,14 +45,12 @@ void onBtnPress() interrupt 0
     TH0 = pitch_TH[pitch_pos];
     TL0 = pitch_TL[pitch_pos];
     TR0 = 1;
-	
-		MoveBool = 0 ;
 }
 
 void onTimer0() interrupt 1 // Speaker oscillator
 {
     TH0 = pitch_TH[pitch_pos];
-    TL0 = pitch_TL[pitch_pos]; 
+    TL0 = pitch_TL[pitch_pos];
     SP = !SP;
 }
 
@@ -62,10 +59,12 @@ void onTimer1() interrupt 3 // Speaker note timer
     if (delaycount > 2)
     {
         TR0 = TR1 = 0;
-			
-        if     (pitch_pos < 19)  pitch_pos++;
-        else    pitch_pos = 0;
-			
+
+        if (pitch_pos < 19)
+            pitch_pos++;
+        else
+            pitch_pos = 0;
+
         delaycount = 0;
         EX0 = 1;
     }
@@ -78,24 +77,11 @@ void onTimer1() interrupt 3 // Speaker note timer
     }
 }
 
-void scan (void)
+void scan(void)
 {
-	int i ;
-	for (i=0 ; i<=8 ; i++)
-	{
-		dotm_put( LEDdot[i][] );
-	}
-	
-}
-
-void main (void)
-{
-	int i ;
-	
-	while(1)
-	{
-		
-		
-		
-	}
+    int i;
+    for (i = 0; i <= 8; i++)
+    {
+        dotm_put(LEDdot[i][]);
+    }
 }
