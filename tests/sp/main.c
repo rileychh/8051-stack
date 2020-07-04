af9a4d2abf4eb0ff7bb77c10c2cb351315d6ac54
WeedForJustice <chengshuan@pm.me>
2020-07-04 10:52:15 +0800

    Try to fix tests/sp

--- tests/sp/main.c
+++ tests/sp/main.c
@@ -1,6 +1,8 @@
 #include <AT89X52.H>
 #include "../../inc/frog.h"
 
+void sp_init();
+void sp_play(u8 sound);
 
 code u8 pitch_TH[] = {
     0xf8, 0xfc, 0xf9, 0xfc, 0xfa, 0xfd, 0xfa, 0xfd, 0xfb,
@@ -8,28 +10,31 @@ code u8 pitch_TH[] = {
 code u8 pitch_TL[] = {
     0x88, 0x44, 0x59, 0xac, 0x13, 0x09, 0x68, 0x34, 0x04,
     0x82, 0x8f, 0xc7, 0x0b, 0x05, 0x22, 0x11, 0x23, 0x46};
-u8 currNote,pitch_pos = 0;
-void sp_init();
-void sp_play(u8 sound);
-u8 pos,i;
-int time;		
+
+u8 currNote = 0, pitch_pos = 0;
+u8 pos = 0, i;
+int time;
+
 void main()
 {
-	while(1)
-	{		
-		sp_init();		
-		time = 1000;
-		
-		if(pos>16)
-			pos = 0;
-		
-		while (time)
-		{
-			for (i = 120; i; i--);
-				time--;
-		}
-	}
+    sp_init();
+
+    for (;;)
+    {
+        if (pos > 16)
+            pos = 0;
+
+        time = 1000;
+
+        while (time)
+        {
+            for (i = 120; i; i--)
+                ;
+            time--;
+        }
+    }
 }
+
 void sp_init()
 {
     TMOD = 0x11;   // Timer 0, 1 are 16 bit mode
@@ -38,11 +43,13 @@ void sp_init()
     IT0 = 1;       // Set interrupt mode to falling edge
     EA = 1;        // Enable main interrupt flag
 }
-void btnPress() interrupt 0  
+
+void btnPress() interrupt 0
 {
-	sp_play(pos);	
-	pos++;
+    sp_play(pos);
+    pos++;
 }
+
 void onTimer0() interrupt 1 // Speaker oscillator
 {
     TR0 = 0;
@@ -60,6 +67,7 @@ void onTimer1() interrupt 3 // Speaker note timer
     currNote++;
     TR1 = 1;
 }
+
 void sp_play(u8 sound)
 {
     currNote = 0;
